7150fdf0c01f63956fa8472931854543905d4f65
beyzaozan <ozan18@itu.edu.tr>
2021-08-02 01:07:05 +0300

    use async/await instead of promise chaining

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,12 +1,26 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
+// // CHAINING
+// Task.findByIdAndDelete('6105ad5eb9cc183ce98f2f5b').then((task) => {
+//     console.log(task + ' is deleted')
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
 
-Task.findByIdAndDelete('6105ad5eb9cc183ce98f2f5b').then((task) => {
-    console.log(task + ' is deleted')
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// ASYNC/AWAIT
+const deleteTaskAndCount = async (id) => {
+    const deleted = await Task.findByIdAndDelete(id)
+    const countIncompleted = await Task.countDocuments({completed: false})
+    return countIncompleted
+}
+
+deleteTaskAndCount('61059bfaff07f03a7282f2b9').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
+
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,37 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('6105aa1904a0333bedfbae96', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({age: 1})  // return the next promise ->chaining
-}).then((result) => {
-    console.log(result)
+// CHAINING
+// User.findByIdAndUpdate('6105aa1904a0333bedfbae96', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age: 1})  // return the next promise ->chaining
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// ASYNC/AWAIT
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age}) // age:age
+    const count = await User.countDocuments({age}) // age:age
+    return count
+}
+
+updateAgeAndCount('6105ab9efaede43c3fc5ae37', 2).then((result) => {
+    console.log('count :',result)
 }).catch((e) => {
     console.log(e)
 })
+
+
+
+
+
+
+
+
+
+// async/ await
+// çoklu fonksiyon kullanımı daha kolay
+// async işlemleri sync muş gibi sıralayıp yapabiliyoruz
