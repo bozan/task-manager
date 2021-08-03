ba8e596fd978f4ebdf0df20b342fe82bfbc12351
beyzaozan <ozan18@itu.edu.tr>
2021-08-03 21:26:18 +0300

    Sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,20 +20,29 @@ router.post('/tasks', auth, async (req, res) => {
 // GET ALL TASKS
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:asc or /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
         const match = {}
+        const sort = {}
 
         if (req.query.completed) {
             match.completed = req.query.completed === 'true'
         }
-    
+        
+        if(req.query.sortBy) {
+            const parts = req.query.sortBy.split(':')
+            sort[parts[0]] = parts[1] == 'desc' ? -1 : 1
+
+        }
+
         try {
             await req.user.populate({
                 path: 'myTasks',
                 match,
                 options: {
                     limit: parseInt(req.query.limit),
-                    skip: parseInt(req.query.skip)
+                    skip: parseInt(req.query.skip),
+                    sort
                 }
             }).execPopulate()
             console.log(req.user)
